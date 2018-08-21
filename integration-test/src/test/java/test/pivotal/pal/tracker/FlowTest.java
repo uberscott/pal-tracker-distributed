2223e1935be0040e3eea9fb528fd0da84db851cf
Scott Williams <scott@mightydevco.com>
2018-08-21 13:24:23 -0600

    testing deploy again

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -60,6 +60,7 @@ public class FlowTest {
         allocationsServer.startWithDatabaseName("tracker_allocations_test");
         backlogServer.startWithDatabaseName("tracker_backlog_test");
         timesheetsServer.startWithDatabaseName("tracker_timesheets_test");
+
         ApplicationServer.waitOnPorts("8881", "8882", "8883", "8884");
         TestScenarioSupport.clearAllDatabases();
     }
